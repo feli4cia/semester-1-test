222cdcda92af9e5f320f9b7ea2c96e84502a1e13
FeliciaCimpoaca <94449695+feli4cia@users.noreply.github.com>
2022-11-25 11:26:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+#q1 csv is easy to create;
+#human readable,ex:the data is not encoded or converted to binary before storing
+#q2 
+#for times in range(5):
+ #  number = input("Please enter a number: ") 
+#number +- ","
+#if times == 0:
+ #  file = open("NumberList.csv","w")
+  # file.write(number)
+   #file.close()
+#else:
+ #    file = open("NumberList.csv","a")
+  #   file.write(number)
+   #  file.close()
+
+#Q3
+#numList.csv = [1, 4, 4, 7, 9]
+# mean = statistics.mean(numList.csv)
+# print("mean: ", mean)
+
+# ##median - middle value
+# median = statistics.median(numList.csv)
+# print("median:", median)
+
+# ##mode - most common/frequent value
+# mode = statistics.mode(numList.csv)
+# print("The mode is:", mode)
+
+
+#Q4
+# print(numList.csv)
+
+#Q5
+#numList.csv = dataIn.split(",")
+# print(numList.csv)
+# numList.csv = [int(item) for item in numList.csv]
+# print(numList.csv)
+
+##Q6
+# print(numList.csv)
+
+#q7
+
+#numList.csv.sort()
+#print(numList.csv)
+
+#Q8
+#print(numList.csv)
+
+#Q9
+# print("Min Value:  ", min(numList.csv))
+#print("Max Value: ",max(numList.csv))
+
+#Q10
+#import statistics
+#average=statistics.mean(numList.csv)
+#print(average)
+
+#names=[]
+#pets=[]
+
+
+#names = input("Whats your name?")
+#pets= int(input("how many pets you have"?"))
+
+#"2."
+
+#"removing unnecessary code that doesnt help me "
+
+#IT KEEPS THE COPE TIDY AND CLEAN  SO THAT OTHER DEVELOPERS CAN WORK FIRELY ON IT
+
+#"3."
+
+#"11111011"
+
+#def is_anagram(w1, w2): 
+
+#if sorted(w1) == sorted(w2): 
+
+#return True 
+
+#else: 
+
+#return False 
+
+ 
+
+word1 = input("Enter the first word: ") 
+
+word2 = input("Enter the second word  ")
+
+word3= "LISTEN"
+
+word4= "SILENT"
+
+if(word1,word2) == (word3,word4):
+  print("YES")
+
+
+if (unsorted(word1)) == (unsorted words)
+print(word1,),"is always an anagram of"(word2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
